20caa8eb3af0d9354241cd4d0016d449e39d2c45
adamparker75 <adamparker75@sky.com>
2021-01-29 19:23:53 +0000

    Create surf lessons view and template

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, SurfLesson
 
 
 def all_products(request):
@@ -12,3 +12,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def surf_lessons(request):
+    """ A view to show all products"""
+
+    surf_lessons = SurfLesson.objects.all()
+
+    context = {
+        'surf_lessons': surf_lessons,
+    }
+
+    return render(request, 'surf_lessons/surf_lessons.html', context)
