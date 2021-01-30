526e64962db49c069a9b81eff5624049757df7e5
adamparker75 <adamparker75@sky.com>
2021-01-30 10:29:25 +0000

    Create Blog model and migrations

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Blog
 
 # Register your models here.
+
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'date',
+        'author',
+    )
+
+
+admin.site.register(Blog, BlogAdmin)
--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=254)
+    body = models.CharField(max_length=800)
+    date = models.DateTimeField(auto_now_add=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
