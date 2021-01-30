0bde1d492adde0fcfeac242c8a087d9fabd840ad
adamparker75 <adamparker75@sky.com>
2021-01-30 11:14:13 +0000

    Add content to blog template as test, modify blog view

--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Blog
 
 
 def blog(request):
     """ A view to return the blog page """
 
-    return render(request, 'blogs/blog.html')
+    blogs = Blog.objects.all().order_by('date')
+
+    context = {
+        'blogs': blogs,
+    }
+
+    return render(request, 'blogs/blog.html', context)
