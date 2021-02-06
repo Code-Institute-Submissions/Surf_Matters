08847a84312923aee38706a9145d4b105c1b99f6
adamparker75 <adamparker75@sky.com>
2021-02-06 13:36:07 +0000

    Add sizes functionality

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,17 +12,33 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        # Get the product
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        # Increment product count
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        # Check if the item_data is an integer
+        if isinstance(item_data, int):
+            # Get the product
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            # Increment product count
+            product_count += item_data
+            # Add dictionary to bag items list
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            # Iterate through items_by_size
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         # Calculate the delivery charge
