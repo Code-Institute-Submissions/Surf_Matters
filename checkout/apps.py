f7e4b21d8b4fccb22290269e8c1f6903d8f9aab2
adamparker75 <adamparker75@sky.com>
2021-02-07 20:24:52 +0000

    Add forms, admin and signals for checkout app

--- checkout/apps.py
+++ checkout/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class CheckoutConfig(AppConfig):
     name = 'checkout'
+
+    # Override the ready module and import signals module
+    def ready(self):
+        import checkout.signals
