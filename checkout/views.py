721365b7495500c5ef29cf9b16a654123a449b6c
adamparker75 <adamparker75@sky.com>
2021-02-13 15:08:26 +0000

    Complete webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -57,7 +57,11 @@ def checkout(request):
         order_form = OrderForm(form_data)
         # If the order form is valis, save the order
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     # Get the product ID out of the bag
