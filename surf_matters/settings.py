5c98035cd43b53b7f54e057615445133acff32ba
adamparker75 <adamparker75@sky.com>
2021-02-17 11:27:43 +0000

    Remove secret key and set Debug

--- surf_matters/settings.py
+++ surf_matters/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ey2l5bc7(2@dmj7fby2hb(^k6a_j%5!7#8k0x62kouxb#v#4!y'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['adamp-surf-matters.herokuapp.com', 'localhost']
 
